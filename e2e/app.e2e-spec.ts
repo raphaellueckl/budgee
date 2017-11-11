fb93cf6e78aec21c59111afbbaa1f452e05b654d
codepleb <raffi.lueckl@gmx.ch>
2017-11-11 03:00:20 -0800

    Moved to CLI 1.6 beta, upgraded to angular 5.0.0

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { BudgeePage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('budgee App', () => {
-  let page: BudgeePage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new BudgeePage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
