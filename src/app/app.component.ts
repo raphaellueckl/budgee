01d0e6d9a94b3dbfe73f4cb23ea841b6d656aab4
codepleb <raffi.lueckl@gmx.ch>
2017-12-31 09:09:55 +0100

    Temp commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatDialog, MatIconRegistry} from '@angular/material';
+import {DomSanitizer} from '@angular/platform-browser';
+import {DialogComponent} from './dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  constructor() { }
+  selectedUser;
+  isDarkTheme = false;
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer, private dialog: MatDialog) {
+    // To avoid XSS attacks, the URL needs to be trusted from inside of your application.
+    // const avatarsSafeUrl = sanitizer.bypassSecurityTrustResourceUrl('./assets/avatars.svg');
+
+    // iconRegistry.addSvgIconSetInNamespace('avatars', avatarsSafeUrl);
+  }
 
   ngOnInit() {
+  }
+
+  openAdminDialog() {
+    this.dialog.open(DialogComponent).afterClosed()
+      .filter(result => !!result)
+      .subscribe(user => {
+        this.users.push(user);
+        this.selectedUser = user;
+      });
   }
 
 }
