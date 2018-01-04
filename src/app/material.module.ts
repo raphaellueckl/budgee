afbe5175f9bb39d6ad7707198b75aa39e8a27eed
codepleb <raffi.lueckl@gmx.ch>
2018-01-04 20:50:49 +0100

    Ng-Table

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,20 +2,22 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {
-MatButtonModule,
-MatCardModule,
-MatCheckboxModule,
-MatDialogModule,
-MatIconModule,
-MatInputModule,
-MatListModule,
-MatMenuModule,
-MatSelectModule,
-MatSidenavModule,
-MatSlideToggleModule,
-MatTabsModule,
-MatToolbarModule
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSlideToggleModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule
 } from '@angular/material';
+import {MatPaginatorModule} from '@angular/material/paginator';
 import 'hammerjs';
 
 @NgModule({
@@ -37,6 +39,8 @@ import 'hammerjs';
     MatSlideToggleModule,
     MatTabsModule,
     MatToolbarModule,
+    MatTableModule,
+    MatPaginatorModule,
     // Flex-layout
     FlexLayoutModule
   ],
