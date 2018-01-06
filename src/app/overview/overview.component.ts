f6a9a812ca89df248ec7622f94964661925a0c6f
raphaellueckl <raphael.lueckl@gmx.ch>
2018-01-06 01:46:46 +0100

    Added routing and animations

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {routerTransition} from '../routing/router-transitions';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.css']
+  styleUrls: ['./overview.component.css'],
+  animations: [routerTransition()]
 })
 export class OverviewComponent implements OnInit {
 
